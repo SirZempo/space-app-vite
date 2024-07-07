42a418efe30aad41e29917d571ae4d8bcc9ddfda
SirZempo <uaemexzempo@gmail.com>
2024-07-07 14:27:27 -0600

    ✅ Se modifico el componente Tags

--- src/components/Galeria/Tags/index.jsx
+++ src/components/Galeria/Tags/index.jsx
@@ -1,7 +1,53 @@
+import tags from "./tags.json";
+import { styled } from "styled-components";
+
+const ContenedorBotones = styled.section`
+    display: flex;
+    align-items: center;
+    gap: 64px;
+    margin-top: 56px;
+`;
+
+const TagTitulo = styled.h3`
+    color: #D9D9D9;
+    font-size: 24px;
+    margin: 0;
+`;
+
+const TagBoton = styled.button`
+    font-size: 24px;
+    border-radius: 10px;
+    border: 2px solid transparent;
+    background-color: #D9D9D94D;
+    color: #FFFFFF;
+    cursor: pointer;
+    display: block;
+    padding: 12px;
+    box-sizing: border-box;
+    &:hover{
+        border-color: #C98CF1;
+    }
+`;
+
+const Div = styled.div`
+    display: flex;
+    gap: 24px;
+    justify-content: end;
+`;
+
 const Tag = () =>{
     return (
         <>
-            <p>Tags</p>
+            <ContenedorBotones>
+                <TagTitulo>Buscar por Tags:</TagTitulo>
+                <Div>
+                    {
+                        tags.map(tag =>{
+                            return <TagBoton key={tag.id}>{tag.titulo}</TagBoton>
+                        })
+                    }
+                </Div>
+            </ContenedorBotones>
         </>
     );
 }
