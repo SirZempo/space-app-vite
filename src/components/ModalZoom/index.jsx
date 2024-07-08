9c730ee0d0a5288f123ddb044a1bbc606044a977
SirZempo <uaemexzempo@gmail.com>
2024-07-08 17:44:08 -0600

    ⚠️ Se requiere corregir el ModalZoom

--- src/components/ModalZoom/index.jsx
+++ src/components/ModalZoom/index.jsx
@@ -1,12 +1,34 @@
+import Imagen from "../Galeria/Imagen";
+import { styled } from "styled-components";
+
+const Overlay = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0,0,0, 0.7);
+`;
+
+const DialogoEstilizado = styled.dialog`
+    position: absolute;
+    top: 24px;
+`;
+
 const ModalZoom = ({foto}) =>{
     return (
         <>
-            <dialog open={!!foto}>
-                <p>Greetings, one and all</p>
-                <form action="" method="dialog">
-                    <button>OK</button>
-                </form>
-            </dialog>
+            {
+                foto && <>
+                    <Overlay></Overlay>
+                    <DialogoEstilizado open={!!foto}>
+                        <Imagen expandida={true} foto={foto}></Imagen>
+                        <form action="" method="dialog">
+                            <button>OK</button>
+                        </form>
+                    </DialogoEstilizado>
+                </>
+            }
         </>
     );
 }
